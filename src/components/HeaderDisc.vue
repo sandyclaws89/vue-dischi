60575cf63189963f50db7017fbdb3f7df65f3df9
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-24 15:58:50 +0100

    Impostazione pag

---
+++ src/components/HeaderDisc.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="row d-flex justify-content-between">
+      <div class="col-6 ">
+          ciao sono header
+      </div>
+      <div class="col-6">
+          ciao sono logo 
+      </div>
+      
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderDisc',
+}
+</script>
+
+<style>
+    header{
+        background-color: grey;
+        height: 50px;
+    }
+</style>
---
+++ src/components/MainDisc.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="row">
+      <div>
+
+      </div>
+     
+  </main>
+</template>
+
+<script>
+// import axios from 'axios';
+export default {
+    name:'MainDisc',
+
+}
+</script>
+
+<style>
+    main{
+        height: calc(100vh - 50px);
+       
+        background-color: cadetblue ;
+    }
+</style>
