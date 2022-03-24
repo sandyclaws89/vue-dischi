d30476df907be53690924d1cda8247b9e472a738
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-24 17:12:54 +0100

    provo axios

---
+++ src/components/CardDisc.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:'CardVue'
+}
+</script>
+
+<style>
+
+</style>
--- src/components/MainDisc.vue
+++ src/components/MainDisc.vue
@@ -8,9 +8,20 @@
 </template>
 
 <script>
-// import axios from 'axios';
+import axios from 'axios';
+
 export default {
     name:'MainDisc',
+    data () {
+        return {
+
+        }
+    },
+    components: {
+        created() {
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        }
+    }
 
 }
 </script>
